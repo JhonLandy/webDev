0690a0b9e5f6eee0ae0d77e6018d986cb3eadbc7
YuanChengLang <709364178@qq.com>
2020-08-22 18:01:34 +0800

    action

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ apt install git
 # 确保脚本抛出遇到的错误
 set -e
 #
-# git clone https://github.com/JhonLandy/dockerDeploy.git 
+git clone https://github.com/JhonLandy/dockerDeploy.git 
 # 生成静态文件
 yarn build:cloundServer 
 # 进入生成的文件夹
 cp -r ./docs/.vuepress/dist ./dockerDeploy/dist 
 cd ./dockerDeploy
-# git config --global user.name JhonLandy
-# git config --global user.email 709364178@qq.com
+git config --global user.name 'JhonLandy'
+git config --global user.email '709364178@qq.com'
 git status
 git add .
 git commit -m 'deploy'
