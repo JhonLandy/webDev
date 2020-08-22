22fc1191bfff891ace9b695c62b81bd8287d66c2
YuanChengLang <709364178@qq.com>
2020-08-22 14:58:41 +0800

    jooks

--- deploy.sh
+++ deploy.sh
@@ -11,14 +11,13 @@ set -e
 
 # cd ../../../
 # 生成静态文件
-yarn build:githubServer 
+npm build:githubServer 
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
-
 git init
 git add .
 git commit -m 'deploy'
