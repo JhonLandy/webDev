79de413a041e5b9b2f9d652db03fc00da4bd36e1
YuanChengLang <709364178@qq.com>
2020-08-22 16:51:56 +0800

    action

--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,30 @@
 
 # 确保脚本抛出遇到的错误
 set -e
-
+#
+git clone https://github.com/JhonLandy/dockerDeploy.git 
 # 生成静态文件
-# yarn build:cloundServer 
+yarn build:cloundServer 
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
-
-# cd ../../../
-# 生成静态文件
-npm run build:githubServer 
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+cp -r docs/.vuepress/dist dockerDeploy/dist 
+cd dockerDeploy
 git init
 git add .
 git commit -m 'deploy'
+# cd ../../../
+# 生成静态文件
+# npm run build:githubServer 
 
-git remote add origin https://github.com/JhonLandy/blog.git
-git push -f https://github.com/JhonLandy/blog.git
+# # 进入生成的文件夹
+# cd docs/.vuepress/dist
 
-cd -
+# # 如果是发布到自定义域名
+# # echo 'www.example.com' > CNAME
+# git init
+# git add .
+# git commit -m 'deploy'
+
+# git remote add origin https://github.com/JhonLandy/blog.git
+# git push -f https://github.com/JhonLandy/blog.git
+
+# cd -
