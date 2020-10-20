53c72f1902a4bbe6190dd4007e210ab89d84b071
wb.yuanchenglang <wb.yuanchenglang@mesg.corp.netease.com>
2020-10-20 17:48:10 +0800

    fix

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,4 +1,4 @@
-import {Carousel, CarouselItem, Dropdown, DropdownMenu, DropdownItem} from 'element-ui'
+import ElementUI from 'element-ui'
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   // options, // 附加到根实例的一些选项
@@ -6,9 +6,10 @@ export default ({
   // siteData, // 站点元数据
   // isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
-  Vue.use(Carousel)
-  Vue.use(CarouselItem)
-  Vue.use(Dropdown)
-  Vue.use(DropdownMenu)
-  Vue.use(DropdownItem)
+  Vue.use(ElementUI)
+  // Vue.use(Carousel)
+  // Vue.use(CarouselItem)
+  // Vue.use(Dropdown)
+  // Vue.use(DropdownMenu)
+  // Vue.use(DropdownItem)
 }
