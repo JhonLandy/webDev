af061ae120f552afd17b19f246276d0feeb92b80
huaweiserver <709364178@qq.com>
2021-06-08 11:42:38 +0800

    fix

--- install.sh
+++ install.sh
@@ -7,15 +7,12 @@ apt install dos2unix
 echo "脚本出错请运行:dos2unix + 文件名"
 apt install docker && apt install docker-compose && service docker start
 apt install git && git config --global user.name "huaweiserver" && git config --global user.email "709364178@qq.com"
-
-
 echo '检查目录是否存在'
 if [ ! -d "/source" ];
 then
     echo '目录不存在，创建目录'
     mkdir source
 fi
-
 git clone https://github.com/JhonLandy/dockerDeploy.git
 echo "部署资源完毕, 启动服务......"
 cd dockerDeploy && sh deploy-dev.sh
