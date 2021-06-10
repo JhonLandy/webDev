688e4dca12e248af6bd3fc9547a7ca7a494fa4e8
Mona <33271955+JhonLandy@users.noreply.github.com>
2021-06-10 11:34:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "开始部署资源"
-cd source
 apt update
 apt install dos2unix
 echo "脚本出错请运行:dos2unix + 文件名"
@@ -12,6 +11,7 @@ then
     echo '目录不存在，创建目录'
     mkdir source
 fi
+cd source
 git clone https://github.com/JhonLandy/dockerDeploy.git
 echo "部署资源完毕, 启动服务......"
 cd dockerDeploy && sh deploy-dev.sh
